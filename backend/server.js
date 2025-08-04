cbe19317bae7362d1873945f3faf1366fb0212c7
praful <praful.wanjari@gmail.com>
2025-08-04 15:52:47 +0530

    upadated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'storeratingproject.netlify.app', // frontend URL
+    origin: 'https://storeratingproject.netlify.app', // frontend URL
     credentials: true, // allow cookies / auth headers
   })
 );
