3e294009fd017413e217a77d6bb2e4d1e4c43e7e
praful <praful.wanjari@gmail.com>
2025-08-04 16:47:13 +0530

    upadated

--- frontend/src/utills/axiosInstance.js
+++ frontend/src/utills/axiosInstance.js
@@ -21,7 +21,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://store-rating-system-lovat.vercel.app/api',
+  baseURL: 'https://store-rating-system-one.vercel.app/api',
   withCredentials: true,
 });
 
